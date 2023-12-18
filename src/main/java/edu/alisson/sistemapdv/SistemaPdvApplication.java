952e9d6e90105679bbb0c88c7d4b508dce8e98ca
Alisson Rego <alissondroid07@gmail.com>
2023-12-18 16:46:56 -0300

    Commit inicial

---
+++ src/main/java/edu/alisson/sistemapdv/SistemaPdvApplication.java
@@ -0,0 +1,13 @@
+package edu.alisson.sistemapdv;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SistemaPdvApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SistemaPdvApplication.class, args);
+	}
+
+}
---
+++ src/test/java/edu/alisson/sistemapdv/SistemaPdvApplicationTests.java
@@ -0,0 +1,13 @@
+package edu.alisson.sistemapdv;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SistemaPdvApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
